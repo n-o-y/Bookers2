11b56dfdb109263978d05f9d1a6691cf80a21f6b
EC2 Default User <ec2-user@ip-172-31-6-123.ec2.internal>
2021-09-27 17:07:55 +0000

    [update]必要なviewページの作成完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resouce)
+    show_user_path(current_user.id)
+  end
+
+  def after_sign_in_path_for(resouce)
+    show_user_path(current_user.id)
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users' => 'users#index', as: "users"
+  get '/user/:id' => 'users#show', as: "show_user" 
+  get '/users/:id/edit' => 'users#edit', as: "edit_user"
   root to: 'homes#top'
+  get '/home/about' => 'homes#about'
   resources :books
 end
